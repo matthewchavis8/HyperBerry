995377994175b09aeaf17b9254a78bd66b15af64
matthewchavis8 <matthewchavis8@gmail.com>
2026-03-25 18:14:26 -0500

    [drivers] Added a Uart driver  - added a PL011 UART driver with a macro to set the base address for    qemu and the physical rpi5  - putc/print implementation using correct PL011 register offset  - added Uart driver to be statically linked  - swapped from C++14 -> C++17 and linked Uart to ELF

--- core/main.cpp
+++ core/main.cpp
@@ -1,7 +1,9 @@
+#include "../drivers/uart/uart.h"
 
 // Stub to see if we can boot and flash the QEMU virtual
 extern "C" void hmain() {
   for (;;) {
+    Uart::print("I have no mouth and I must scream:(\n");
     asm volatile("wfe");
   }
 }
