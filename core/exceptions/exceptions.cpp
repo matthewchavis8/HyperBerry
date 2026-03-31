16fc47dce2ddd1ddb8cf96e02e8d8999a517dc46
matthewchavis8 <matthewchavis8@gmail.com>
2026-03-30 22:41:57 -0500

    [core] added handler stubs for each exception group  - added exception handler stubs just to get it to compile for right now will work    on logic later  - Confirmed image was able to build

--- core/exceptions/exceptions.cpp
+++ core/exceptions/exceptions.cpp
@@ -1,4 +1,17 @@
 #include "exceptions.h"
 #include "uart.h"
 
-extern "C" void lower_el2_sync(ExceptionContext& ex);
+// Hypervisor (EL2) exception handlers
+extern "C" void handle_el2_sync(ExceptionContext& ex) {}
+extern "C" void handle_el2_irq(ExceptionContext& ex) {}
+extern "C" void handle_el2_fiq(ExceptionContext& ex) {}
+extern "C" void handle_el2_serror(ExceptionContext& ex) {}
+
+// Lower EL (guest) exception handlers
+extern "C" void handle_lower_el_sync(ExceptionContext& ex) {}
+extern "C" void handle_lower_el_irq(ExceptionContext& ex) {}
+extern "C" void handle_lower_el_fiq(ExceptionContext& ex) {}
+extern "C" void handle_lower_el_serror(ExceptionContext& ex) {}
+
+// Catch-all for unhandled exceptions
+extern "C" void handle_unhandled(ExceptionContext& ex) {}
