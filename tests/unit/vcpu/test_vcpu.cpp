a0b6c10634a48a2dd08c09c1e9c71c37f2865f33
matthewchavis8 <matthewchavis8@gmail.com>
2026-04-22 16:46:42 -0500

    [tests] extend Vcpu unit tests   - skipInstruction, setPc, GPR round-trips   - HvContext size/offset contract   - getId/setId round-trip   - remove unused strings.h include

--- tests/unit/vcpu/test_vcpu.cpp
+++ tests/unit/vcpu/test_vcpu.cpp
@@ -6,7 +6,6 @@
 #include <gtest/gtest.h>
 
 #include "core/vcpu/vcpu.h"
-#include "lib/strings/strings.h"
 
 static constexpr uint64_t SPSR_EL1H_ALL_MASKED =
     (0b00101ULL) |
@@ -50,6 +49,10 @@ Vcpu* Vcpu::getCurrentVcpu() {
 
 void Vcpu::scheduleNext() {}
 
+void Vcpu::setGuestSp(uint64_t sp) {
+  m_el1SysRegs.regs[regIdx(VCPU_SP_EL1)] = sp;
+}
+
 TEST(Vcpu, IsStandardLayout) {
   EXPECT_TRUE(__is_standard_layout(Vcpu));
 }
@@ -79,4 +82,46 @@ TEST(Vcpu, InitZeroesGpRegs) {
 
 TEST(Vcpu, CurrentReturnsNullOnHostedBuilds) {
   EXPECT_EQ(Vcpu::getCurrentVcpu(), nullptr);
+}
+
+TEST(Vcpu, SkipInstructionAdvancesElrByFour) {
+  Vcpu vcpu;
+  vcpu.init(0x40001000ULL);
+  vcpu.skipInstruction();
+  EXPECT_EQ(vcpu.getElr(), 0x40001004ULL);
+}
+
+TEST(Vcpu, SetPcRoundTrips) {
+  Vcpu vcpu;
+  vcpu.init(0x40000000ULL);
+  vcpu.setPc(0xDEAD0000ULL);
+  EXPECT_EQ(vcpu.getElr(), 0xDEAD0000ULL);
+}
+
+TEST(Vcpu, SetGpRegRoundTripsAcrossMultipleOffsets) {
+  Vcpu vcpu;
+  vcpu.init(0x40000000ULL);
+  vcpu.setGpReg(VCPU_GPREG_X0,  0x1111111111111111ULL);
+  vcpu.setGpReg(VCPU_GPREG_X15, 0x2222222222222222ULL);
+  vcpu.setGpReg(VCPU_GPREG_X29, 0x3333333333333333ULL);
+  vcpu.setGpReg(VCPU_GPREG_LR,  0x4444444444444444ULL);
+  EXPECT_EQ(vcpu.getGpReg(VCPU_GPREG_X0),  0x1111111111111111ULL);
+  EXPECT_EQ(vcpu.getGpReg(VCPU_GPREG_X15), 0x2222222222222222ULL);
+  EXPECT_EQ(vcpu.getGpReg(VCPU_GPREG_X29), 0x3333333333333333ULL);
+  EXPECT_EQ(vcpu.getGpReg(VCPU_GPREG_LR),  0x4444444444444444ULL);
+}
+
+TEST(Vcpu, HvContextSizeMatchesAsmConstant) {
+  EXPECT_EQ(sizeof(HvContext), (size_t)VCPU_HVCTX_SIZE);
+}
+
+TEST(Vcpu, HvContextOffsetMatchesAsmConstant) {
+  EXPECT_EQ(VcpuLayoutAccess::hvCtxOffset(), (uint64_t)VCPU_HVCTX_OFFSET);
+}
+
+TEST(Vcpu, SetIdGetIdRoundTrip) {
+  Vcpu vcpu;
+  vcpu.init(0x40000000ULL);
+  vcpu.setId(42);
+  EXPECT_EQ(vcpu.getId(), 42U);
 }
