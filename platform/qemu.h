44b224dc6df47fb4efa252970120d88c0901a629
matthewchavis8 <matthewchavis8@gmail.com>
2026-04-22 17:32:37 -0500

    [docs] update docs

--- platform/qemu.h
+++ platform/qemu.h
@@ -1,11 +1,19 @@
+/**
+ * @file qemu.h
+ * @brief Platform constants for the QEMU `virt` machine.
+ * @ingroup platform
+ */
 #ifndef __PLATFORM_QEMU_H__
 #define __PLATFORM_QEMU_H__
 
 #include <stdint.h>
 
 namespace Platform {
+/** @brief PL011 UART MMIO base for QEMU `virt`. */
 inline constexpr uint64_t kUartBase = 0x09000000ULL;
+/** @brief Start of the device-mapped MMIO aperture used by the hypervisor. */
 inline constexpr uint64_t kPeripheralBase = 0x08000000ULL;
+/** @brief Size of the MMIO aperture identity-mapped for peripherals. */
 inline constexpr uint64_t kPeripheralSize = 0x38000000ULL;
 }
 
--- platform/rpi5.h
+++ platform/rpi5.h
@@ -1,11 +1,19 @@
+/**
+ * @file rpi5.h
+ * @brief Platform constants for Raspberry Pi 5 hardware.
+ * @ingroup platform
+ */
 #ifndef __PLATFORM_RPI5_H__
 #define __PLATFORM_RPI5_H__
 
 #include <stdint.h>
 
 namespace Platform {
+/** @brief PL011 UART MMIO base on BCM2712. */
 inline constexpr uint64_t kUartBase = 0x107D001000ULL;
+/** @brief Peripheral MMIO base mapped by the EL2 host stage-1 MMU. */
 inline constexpr uint64_t kPeripheralBase = 0x107D000000ULL;
+/** @brief Size of the peripheral MMIO aperture reserved for device mappings. */
 inline constexpr uint64_t kPeripheralSize = 0x200000000ULL;
 }
 
