0d04deb1fd9c2b9fb1dba52ecca8d4b200d9ce6f
matthewchavis8 <matthewchavis8@gmail.com>
2026-04-25 12:13:00 -0500

    [lib] enable registerdump outside of exceptionhandlers  - Added the ability to use register dump outside of exception context    by adding a function overload that take in only the msg. Of course debugging output    will be less verbose without the exception context dump but it is still something

--- lib/panic/panic.cpp
+++ lib/panic/panic.cpp
@@ -16,17 +16,51 @@
  * @param ctx Saved exception context for diagnostic output.
  */
 [[noreturn]] void hv_panic(const char *msg, ExceptionContext& ctx) {
-  Uart::putc('\n');
   Uart::println("=======================================");
   Uart::println("=             HV PANIC                =");
   Uart::println("=======================================");
-  Uart::putc('\n');
 
-  if (msg) {
-    Uart::println("[ERROR]{}", msg);
-  }
+  if (msg)
+    Uart::println("[ERROR] {}", msg);
 
   registerDump(ctx);
+
+  for (;;) {
+    asm volatile("wfe");
+  }
+}
+
+/**
+ * @brief Print panic diagnostics and stop execution permanently.
+ * @ingroup lib
+ * @param msg Optional panic message to print.
+ */
+[[noreturn]] void hv_panic(const char *msg) {
+  Uart::println("=======================================");
+  Uart::println("=             HV PANIC                =");
+  Uart::println("=======================================");
+
+  if (msg)
+    Uart::println("[ERROR] {}", msg);
+
+  // Dump System Register State
+  uint64_t esr  {};
+  uint64_t elr  {};
+  uint64_t far  {};
+  uint64_t spsr {};
+
+  asm volatile("mrs %0, esr_el2"  : "=r"(esr));
+  asm volatile("mrs %0, elr_el2"  : "=r"(elr));
+  asm volatile("mrs %0, far_el2"  : "=r"(far));
+  asm volatile("mrs %0, spsr_el2" : "=r"(spsr));
+
+  uint64_t ec  = (esr >> 26) & 0x3F;
+  uint64_t iss = esr & 0x1FFFFFF;
+
+  Uart::println("[ESR_EL2] {:x} EC={:x} ISS={:x}", esr, ec, iss);
+  Uart::println("[ELR_EL2] {:x}", elr);
+  Uart::println("[FAR_EL2] {:x}", far);
+  Uart::println("[SPSR_EL2] {:x}", spsr);
 
   for (;;) {
     asm volatile("wfe");
